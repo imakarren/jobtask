a5366a8d24f0638b6ffad875407bc38d78929570
imakaren <irina.s.popova@yandex.ru>
2021-07-15 15:23:42 +0300

    Выполнено задание для Перфоманс Лаб

---
+++ task1/Main.java
@@ -0,0 +1,87 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.print("Введите полный путь к файлу :");
+        Scanner scan = new Scanner(System.in);
+        String path = scan.nextLine();
+        int[] buf = fileToSortedMass(path);
+
+        String pattern = "#0.00";
+        DecimalFormat df = new DecimalFormat(pattern);
+
+        System.out.print(df.format(percentile90(buf)).replace(",",".") + "\n" + df.format(median(buf)).replace(",",".") + "\n" +
+                df.format(max(buf)).replace(",",".") + "\n" + df.format(min(buf)).replace(",",".") + "\n" +
+                df.format(average(buf)).replace(",","."));
+
+
+    }
+
+    /*
+     *Represents data from a file as an sorted array.
+     */
+    private static int[] fileToSortedMass (String path) throws IOException {
+        File file = new File(path);
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        int counter = 0;
+        int[] buf = new int[1000];
+
+        while (bufferedReader.ready()) {
+            buf[counter++] = Integer.parseInt(bufferedReader.readLine());
+        }
+
+        if (counter < buf.length) {
+            buf = Arrays.copyOf(buf,counter);
+        }
+        Arrays.sort(buf);
+        return buf;
+    }
+
+    private static double percentile90 (int[] data) {
+        double r = 0.9*(data.length-1);
+        int n = (int) Math.floor(r);
+        double percentile = data[n] + r%1 * (data[n+1] - data[n]);
+        return percentile;
+    }
+
+    private static double median (int[] data) {
+        double median;
+        if (data.length%2 == 0)
+            median = (data[data.length/2 - 1] + data[data.length/2])/2;
+        else median = data[data.length/2];
+        return median;
+    }
+
+    private static int max (int[] data) {
+        int max = data[0];
+        for (int i = 1; i < data.length; i++)
+            if (data[i] > max)
+                max = data[i];
+        return max;
+    }
+
+    private static int min (int[] data) {
+        int min = data[0];
+        for (int i = 1; i < data.length; i++)
+            if (data[i] < min)
+                min = data[i];
+        return min;
+    }
+
+    private static double average (int[] data) {
+        int sum = 0;
+        for (int value : data)
+            sum += value;
+        return (double) sum/data.length;
+    }
+}
---
+++ task2/Main.java
@@ -0,0 +1,77 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Введите полный путь к файлу c координатами четырехугольника: ");
+        Scanner scan = new Scanner(System.in);
+        String path1 = scan.nextLine();
+        System.out.print("Введите полный путь к файлу c координатами точек: ");
+        String path2 = scan.nextLine();
+
+
+        float[][] vert = fileToMass(path1);
+        float[][] point = fileToMass(path2);
+        for (int i = 0; i < point.length; i++)
+            System.out.print(pointPosition(vert, point[i][0],point[i][1]) + "\n");
+    }
+
+    private static int pointPosition (float[][] vert, float pointX, float pointY) {
+        int i, j, count = 0;
+        for (j = 0, i = 3; j < 4; i = j++) {
+            if ( (pointX == vert[i][0]) & (pointY == vert[i][1]) )
+                return 0;
+            else if ( isPointOnBorder(vert, pointX, pointY))
+                return 1;
+            else if ( ((vert[j][0]-vert[i][0])*(pointY-vert[i][1]) - (vert[j][1]-vert[i][1])*(pointX-vert[i][0])) > 0 )
+                count++;
+        }
+        if (count != 4 & count != 0)
+            return 3;
+        return 2;
+    }
+
+    private static boolean isPointOnBorder (float[][] vert, float pointX, float pointY) {
+        int i, j, count = 0, count0 = 0;
+        for (j = 0, i = 3; j < 4; i = j++)
+            if ( ((vert[j][0]-vert[i][0])*(pointY-vert[i][1]) - (vert[j][1]-vert[i][1])*(pointX-vert[i][0])) == 0 )
+                count0++;
+            else if ( ((vert[j][0]-vert[i][0])*(pointY-vert[i][1]) - (vert[j][1]-vert[i][1])*(pointX-vert[i][0])) > 0 )
+                count++;
+        if (count0 == 1 && (count == 3 | count == 0) )
+            return true;
+
+        return false;
+    }
+
+    private static float[][] fileToMass (String path) throws IOException {
+        File file = new File(path);
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        int counter = 0;
+        String[] splitLine;
+        float[][] buf = new float[100][2];
+
+        while (bufferedReader.ready()) {
+             splitLine = bufferedReader.readLine().split(" ");
+
+             for (int j = 0; j < 2; j++)
+                 buf[counter][j] = Float.parseFloat(splitLine[j]);
+
+             counter++;
+        }
+
+        if (counter < buf.length) {
+            buf = Arrays.copyOf(buf,counter);
+        }
+
+        return buf;
+    }
+}
